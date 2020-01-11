e8e68726c7fc889a76a87318c8cead9f1e360431
Khoa Trần <khoatranprogrammer@gmail.com>
2020-01-12 06:52:50 +0700

    [20200112] Update KOCQPluginBase to add Icon
    Detail:
     - Add method addIcon

--- core/KOCQCore/KOCQPluginBase.cpp
+++ core/KOCQCore/KOCQPluginBase.cpp
@@ -4,6 +4,30 @@ KOCQPluginBase::KOCQPluginBase(QQmlEngine* engine, QObject* parent, QUrl source)
 {
     m_engine = engine;
     m_source = source;
+    m_quickItemIcon = NULL;
     QObject::connect(parent, SIGNAL(qmlSignal(QVariant)),
-                       this, SLOT(cppSlot(QVariant)));
+                       this, SLOT(addIconSlot(QVariant)));
+}
+
+QQmlEngine* KOCQPluginBase::getEngine() const
+{
+    return m_engine;
+}
+
+QUrl KOCQPluginBase::getUrlSource() const
+{
+    return m_source;
+}
+
+void KOCQPluginBase::addIcon(const QVariant &v, QString data)
+{
+    QQuickItem *parent = qobject_cast<QQuickItem*>(v.value<QObject*>());
+
+    if(m_quickItemIcon == NULL)
+    {
+        QQmlComponent *component = new QQmlComponent(getEngine());
+        component->setData(data.toUtf8(), getUrlSource());
+        m_quickItemIcon = qobject_cast<QQuickItem*>(component->create());
+        m_quickItemIcon->setParentItem(parent);
+    }
 }
--- core/KOCQCore/KOCQPluginBase.h
+++ core/KOCQCore/KOCQPluginBase.h
@@ -14,12 +14,18 @@ class KOCQCORESHARED_EXPORT KOCQPluginBase : public QObject
 public:
     KOCQPluginBase(QQmlEngine* engine, QObject* parent, QUrl source);
 
+public:
+    QQmlEngine* getEngine() const;
+    QUrl getUrlSource() const;
+    void addIcon(const QVariant &v, QString data);
+
 public slots:
     virtual void addIconSlot(const QVariant &v) = 0;
 
 private:
     QQmlEngine* m_engine;
     QUrl m_source;
+    QQuickItem *m_quickItemIcon;
 };
 
 #endif // KOCQPLUGINBASE_H
