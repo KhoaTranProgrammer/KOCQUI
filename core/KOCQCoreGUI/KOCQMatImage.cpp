503296bfdcba112d45ec48b0e5374d9a824cf5c3
Khoa Trần <khoatranprogrammer@gmail.com>
2020-03-05 15:51:42 +0700

    [20200305][KOCQCoreGUI] Display FullScreen
    Detail:
      - In method paint() of class KOCQMatImage. Update
        algorithm to display fullscreen

--- core/KOCQCoreGUI/KOCQMatImage.cpp
+++ core/KOCQCoreGUI/KOCQMatImage.cpp
@@ -10,7 +10,12 @@ void KOCQMatImage::paint(QPainter *painter)
 {
     if(isImage == true)
     {
-        QRectF r1(0, 0, m_image.width(), m_image.height());
+        float scale = (this->width() / m_image.width()) < (this->height() / m_image.height()) ?
+                    (this->width() / m_image.width()) : (this->height() / m_image.height());
+        float startX, startY;
+        startX = this->width() / 2 - (m_image.width() * scale) / 2;
+        startY = this->height() / 2 - (m_image.height() * scale) / 2;
+        QRectF r1(startX, startY, m_image.width() * scale, m_image.height() * scale);
         painter->drawImage(r1, m_image);
     }
     else
